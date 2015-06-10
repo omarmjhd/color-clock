605e39f42b8d7235cf1992f58313a731948cb0be
Omar Mujahid <omar_mjhd@yahoo.com>
2015-06-09 20:27:36 -0700

    Updated JS and HTML to add link to color-hex.com to see the details of the current hex color

--- js/clock.js
+++ js/clock.js
@@ -25,13 +25,17 @@ function clock() {
 
     hours.toString();
     minutes.toString();
-    seconds.toString();
+    seconds.toString(); //update to change it to 12h clock and AM/PM + current City
 
     var hex = "#" + hours + minutes + seconds;
     var time = hours + ":" + minutes + ":" + seconds;
+    var hexLink = "http://www.color-hex.com/color/" + hours + minutes + seconds;
 
-    document.getElementById("time").innerHTML = time;
+    document.getElementById("time").innerHTML = time; //setting the time and hex values
     document.getElementById("hex").innerHTML = hex;
+
+    document.getElementById("hex").setAttribute("href", hexLink); //testing setting the href
+
     document.body.style.background = hex;
 
     setTimeout(function() { clock();}, 1000);
